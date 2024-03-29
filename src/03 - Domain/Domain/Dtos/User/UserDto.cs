b70d0fb905adceb11aa94d1c92e08aa0bcf9ce5a
Denis Developer <denispereiradosreis@gmail.com>
2024-03-29 18:48:12 -0300

    mudando para class

--- src/03 - Domain/Domain/Dtos/User/UserDto.cs
+++ src/03 - Domain/Domain/Dtos/User/UserDto.cs
@@ -1,6 +1,10 @@
 ﻿namespace Domain.Dtos.User
 {
-    public record UserDto(string Email, string Password);
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 
     public record UserInfoDto(string Email, bool IsAdmin);
 }
