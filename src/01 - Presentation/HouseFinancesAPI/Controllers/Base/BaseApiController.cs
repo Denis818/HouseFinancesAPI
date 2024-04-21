922dbfc743630765c4b306f5dbc76c043c31181e
Denis Developer <denispereiradosreis@gmail.com>
2024-04-21 19:58:55 -0300

    validando model state

--- src/01 - Presentation/HouseFinancesAPI/Controllers/Base/BaseApiController.cs
+++ src/01 - Presentation/HouseFinancesAPI/Controllers/Base/BaseApiController.cs
@@ -3,6 +3,7 @@ using Domain.Enumeradores;
 using Domain.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace HouseFinancesAPI.Controllers.Base
 {
@@ -15,6 +16,12 @@ namespace HouseFinancesAPI.Controllers.Base
 
         protected void Notificar(EnumTipoNotificacao tipo, string message) =>
             _notifier.Notify(tipo, message);
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(!ValidarModelState(context))
+                return;
+        }
 
         public override void OnActionExecuted(ActionExecutedContext context)
         {
@@ -43,6 +50,77 @@ namespace HouseFinancesAPI.Controllers.Base
             _notifier.HasNotifications(EnumTipoNotificacao.Informacao, out var infoMessages);
             return Ok(new ResponseDTO<TResponse>(content) { Mensagens = infoMessages });
         }
+
+        private bool ValidarModelState(ActionExecutingContext context)
+        {
+            var modelState = context.ModelState;
+            if(!modelState.IsValid)
+            {
+                if(!ValidarContentTypeRequest(modelState, context))
+                    return false;
+
+                var valoresInvalidosModelState = modelState.Where(x =>
+                    x.Value.ValidationState == ModelValidationState.Invalid
+                );
+                if(!valoresInvalidosModelState.Any())
+                    return true;
+
+                ExtrairMensagensDeErroDaModelState(valoresInvalidosModelState, context);
+                return false;
+            }
+            return true;
+        }
+
+        private void ExtrairMensagensDeErroDaModelState(
+        IEnumerable<KeyValuePair<string, ModelStateEntry>> valoresInvalidosModelState,
+        ActionExecutingContext context
+    )
+        {
+            var listaErros = new List<Notificacao>();
+            foreach(var model in valoresInvalidosModelState)
+            {
+                var nomeCampo = model.Key.StartsWith("$.") ? model.Key.Substring(2) : model.Key;
+                listaErros.Add(
+                    new Notificacao(
+                        $"Campo {nomeCampo} não está num formato válido.",
+                        EnumTipoNotificacao.ClientError
+                    )
+                );
+            }
+
+            context.Result = new BadRequestObjectResult(
+                new ResponseDTO<string>(null, listaErros.ToArray())
+            );
+        }
+
+        private bool ValidarContentTypeRequest(
+            ModelStateDictionary modelState,
+            ActionExecutingContext context
+        )
+        {
+            var valoresInvalidosModelState = modelState.Where(x =>
+                x.Value.ValidationState == ModelValidationState.Invalid
+            );
+            if(valoresInvalidosModelState.Count() == 1)
+            {
+                var model = valoresInvalidosModelState.First();
+                var modelErro = model.Value.Errors.FirstOrDefault();
+
+                if(model.Key == string.Empty && modelErro.ErrorMessage == string.Empty)
+                {
+                    var result = new ResponseDTO<object>();
+                    result.ContentTypeInvalido();
+                    context.Result = new BadRequestObjectResult(result);
+                    return false;
+                }
+                else if(model.Key == string.Empty)
+                {
+                    model.Value.ValidationState = ModelValidationState.Valid;
+                    return true;
+                }
+            }
+            return true;
+        }
     }
 
     public class ResponseDTO<T>
@@ -57,5 +135,14 @@ namespace HouseFinancesAPI.Controllers.Base
         }
 
         public ResponseDTO() { }
+
+        public void ContentTypeInvalido()
+        {
+            Mensagens =
+            [
+                new("Content-Type inválido.", EnumTipoNotificacao.ClientError)
+            ];
+        }
+
     }
 }
