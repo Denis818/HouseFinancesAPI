bdc1b507686b94012f2013578a3eb9db95d82867
Denis Developer <percap.railway@gmail.com>
2024-05-15 10:59:54 -0300

    correção na rota

--- src/External/Presentation/Api/V2/ProdutoController.cs
+++ src/External/Presentation/Api/V2/ProdutoController.cs
@@ -7,20 +7,16 @@ namespace Presentation.Api.V2
 {
     [ApiController]
     [ApiVersion(ApiVersioning.V2)]
-    [Route("api/v2/categoria")]
+    [Route("api/v2/produto")]
     public class ProdutoController : MainController
     {
-        public ProdutoController(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-        }
+        public ProdutoController(IServiceProvider serviceProvider)
+            : base(serviceProvider) { }
 
         [HttpGet]
         public IEnumerable<object> GetAsync()
         {
-            return new object[]
-            {
-
-            };
+            return new object[] { };
         }
     }
 }
